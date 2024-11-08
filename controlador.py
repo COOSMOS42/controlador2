d2f683196953c529bf6a6a9aa71129ccfac69f9a
João Gabriel <91282960+COOSMOS42@users.noreply.github.com>
2024-11-08 14:08:55 -0300

    Update controlador.py

--- controlador.py
+++ controlador.py
@@ -77,14 +77,14 @@ with st.sidebar:
 with col1:
     
     st.subheader('Status')
-        status = st.selectbox('Escolha o Status', ('Entrega SS', 'Envio à prefeitura', 'Recebimento Revisão',
-                                                         'Assinado pela SS', 'Reenvio à Prefeitura','Concluido', 'Outro') )
+    status = st.selectbox('Escolha o Status', ('Entrega SS', 'Envio à prefeitura', 'Recebimento Revisão',
+                                                     'Assinado pela SS', 'Reenvio à Prefeitura','Concluido', 'Outro') )
 
-        status2 = None
-        if status == 'Outro':
-            status2 = st.text_input('Qual o status?')
-        else:
-            status2 = status
+    status2 = None
+    if status == 'Outro':
+        status2 = st.text_input('Qual o status?')
+    else:
+        status2 = status
     
     # formulário para preenchimento dos dados que serão inputados na lista cache
     with st.form('Preencha os dados', clear_on_submit=True, border=True):
